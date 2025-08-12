a58000ade8d89e81a654ed36cb9a0fb5bc8247f1
[dyad] <itzanu.cyber@gmail.com>
2025-08-12 22:38:36 +0530

    [dyad] Fixed automatic theme cycling - wrote 1 file(s)

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -8,19 +8,26 @@ export function ThemeSwitcher() {
 
   React.useEffect(() => {
     const themes = ["tech-noir", "onyx-neon"];
-    const lastTheme = localStorage.getItem("last-selected-theme");
+    // Read the same storage key that next-themes uses ('theme' by default).
+    // The value can be stored with quotes (e.g., '"tech-noir"'), so we remove them.
+    const lastTheme = localStorage.getItem("theme")?.replace(/"/g, '');
+    
     let nextThemeIndex = 0;
 
     if (lastTheme) {
       const lastIndex = themes.indexOf(lastTheme);
-      nextThemeIndex = (lastIndex + 1) % themes.length;
+      // If the last theme is found in our array, calculate the next index.
+      // Otherwise, we'll default to the first theme.
+      if (lastIndex !== -1) {
+        nextThemeIndex = (lastIndex + 1) % themes.length;
+      }
     }
 
     const newTheme = themes[nextThemeIndex];
     setTheme(newTheme);
-    localStorage.setItem("last-selected-theme", newTheme);
-  }, [setTheme]);
 
-  // This component no longer renders any UI as it's for automatic switching
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []); // We only want this to run once when the component mounts.
+
   return null;
 }
